72950576d515f7cf834882aa24a5235628159aa8
Nishanth Posa <nishanth.posa10@gmail.com>
2026-04-23 19:25:17 +0530

    Refactor to use existing Azure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,33 +12,29 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-chennai-coastal"
-  location = "southindia"
+# Reference existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "rg-chennai-coastal"
 }
 
-# Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = "coastaltiles"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# Reference existing Storage Account
+data "azurerm_storage_account" "storage" {
+  name                = "coastaltiles"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Blob Container
 resource "azurerm_storage_container" "container" {
   name                  = "coastal-tiles"
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id    = data.azurerm_storage_account.storage.id
   container_access_type = "private"
 }
 
-# Azure Functions Plan (Consumption)
+# Azure Functions Plan
 resource "azurerm_service_plan" "plan" {
   name                = "coastal-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
@@ -46,11 +42,11 @@ resource "azurerm_service_plan" "plan" {
 # Azure Functions App
 resource "azurerm_linux_function_app" "func" {
   name                = "func-cloud-removal"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.plan.id
-  storage_account_name = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  storage_account_name = data.azurerm_storage_account.storage.name
+  storage_account_access_key = data.azurerm_storage_account.storage.primary_access_key
 
   site_config {}
 }
